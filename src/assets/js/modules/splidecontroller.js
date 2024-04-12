938686b07bc5bb96d3c9c1feedfc4062f46ef5eb
Bruno Galvão Martins <bruno.martins@client-craft.com>
2024-04-12 18:21:51 -0300

    fix: script for slides

--- src/assets/js/modules/splidecontroller.js
+++ src/assets/js/modules/splidecontroller.js
@@ -27,25 +27,3 @@ slides.forEach((slide) => {
 
   slideTreatment.mount();
 });
-
-// const slideTreatment = new Splide(slide1, {
-//   type: "loop",
-//   gap: "4rem",
-//   padding: "4rem",
-//   perPage: 3,
-//   arrows: false,
-//   autoplay: true,
-//   rewind: true,
-//   breakpoints: {
-//     920: {
-//       perPage: 2,
-//       arrows: true,
-//     },
-//     720: {
-//       perPage: 1,
-//       arrows: true,
-//     },
-//   },
-// });
-
-// slideTreatment.mount();
