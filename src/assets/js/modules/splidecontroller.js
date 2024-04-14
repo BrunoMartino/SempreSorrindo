cae23e3068213ce42209454360b93dcf71f58cc4
Bruno Galvão Martins <bruno.martins@client-craft.com>
2024-04-14 16:02:21 -0300

    create pages for single-clinic

--- src/assets/js/modules/splidecontroller.js
+++ src/assets/js/modules/splidecontroller.js
@@ -6,21 +6,26 @@ console.log(slides);
 slides.forEach((slide) => {
   const slideTreatment = new Splide(slide, {
     type: "loop",
-    gap: "4rem",
+    gap: "5rem",
     padding: "4rem",
     perPage: 3,
     arrows: false,
     autoplay: true,
     rewind: true,
+    pagination: false,
+    lazyLoad: true,
     breakpoints: {
       920: {
         perPage: 2,
-        arrows: true,
+        arrows: false,
+        lazyLoad: true,
       },
       720: {
         perPage: 1,
         arrows: true,
-        padding: "1.5rem",
+        padding: "2rem",
+        gap: "3rem",
+        lazyLoad: true,
       },
     },
   });
