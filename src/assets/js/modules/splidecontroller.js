bdadefa22088bd651f27100034b1d903213b310a
Bruno Galvão Martins <bruno.martins@client-craft.com>
2024-04-16 10:33:26 -0300

    fix: fixes padding on slides

--- src/assets/js/modules/splidecontroller.js
+++ src/assets/js/modules/splidecontroller.js
@@ -1,39 +1,126 @@
 import Splide from "@splidejs/splide";
 
-const slides = document.querySelectorAll("[data-home='slide']");
-console.log(slides);
+const slide1 = document.querySelector("[data-home='slide_1']");
 
-slides.forEach((slide) => {
-  const slideTreatment = new Splide(slide, {
-    type: "loop",
-    gap: "5rem",
-    padding: "4rem",
-    perPage: 3,
-    arrows: false,
-    autoplay: true,
-    rewind: true,
-    pagination: false,
-    lazyLoad: true,
-    breakpoints: {
-      1024: {
-        perPage: 2,
-        arrows: false,
-        lazyLoad: true,
-      },
-      920: {
-        perPage: 2,
-        arrows: false,
-        lazyLoad: true,
-      },
-      720: {
-        perPage: 1,
-        arrows: false,
-        padding: "4rem",
-        gap: "3rem",
-        lazyLoad: true,
-      },
+// controls Slide for treatments
+const slideTreatment = new Splide(slide1, {
+  autoplay: true,
+  type: "loop",
+  rewind: true,
+  gap: "2rem",
+  perMove: 1,
+  pagination: false,
+  arrows: false,
+  breakpoints: {
+    1440: {
+      perPage: 3,
     },
-  });
-
-  slideTreatment.mount();
+    980: {
+      perPage: 2,
+    },
+    720: {
+      perPage: 1,
+    },
+  },
 });
+slideTreatment.mount();
+
+const slide2 = document.querySelector("[data-home='slide_2']");
+
+// controls slides for offices
+const slideOffices = new Splide(slide2, {
+  autoplay: true,
+  type: "loop",
+  rewind: true,
+  gap: "2rem",
+  perMove: 1,
+  pagination: false,
+  arrows: false,
+  breakpoints: {
+    1440: {
+      perPage: 3,
+    },
+    980: {
+      perPage: 2,
+    },
+    720: {
+      perPage: 1,
+    },
+  },
+});
+slideOffices.mount();
+
+const slide3 = document.querySelector("[data-home='slide_3']");
+
+// control slides for prizes
+const slidePrizes = new Splide(slide3, {
+  autoplay: true,
+  type: "loop",
+  rewind: true,
+  gap: "2rem",
+  perMove: 1,
+  pagination: false,
+  arrows: false,
+  breakpoints: {
+    1440: {
+      perPage: 3,
+    },
+    980: {
+      perPage: 2,
+    },
+    720: {
+      perPage: 1,
+    },
+  },
+});
+slidePrizes.mount();
+
+const slide4 = document.querySelector("[data-home='slide_4']");
+
+// constrol slides for News
+const slideNews = new Splide(slide4, {
+  autoplay: true,
+  type: "loop",
+  rewind: true,
+  gap: "4rem",
+  perMove: 1,
+  pagination: false,
+  arrows: false,
+  breakpoints: {
+    1440: {
+      perPage: 2,
+    },
+    980: {
+      perPage: 2,
+    },
+    720: {
+      perPage: 1,
+    },
+  },
+});
+slideNews.mount();
+
+const slide5 = document.querySelector("[data-home='slide_5']");
+
+// constrol slides for reviews
+const slideReviews = new Splide(slide5, {
+  autoplay: true,
+  type: "loop",
+  rewind: true,
+  gap: "4rem",
+  perMove: 1,
+  pagination: false,
+  arrows: false,
+  breakpoints: {
+    1440: {
+      perPage: 2,
+    },
+    980: {
+      perPage: 2,
+    },
+    720: {
+      perPage: 1,
+    },
+  },
+});
+slideReviews.mount();
